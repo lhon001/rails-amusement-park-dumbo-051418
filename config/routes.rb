230bc1fb32186070b9f3cbaa02707b7854bf89bc
github username <github email address>
2018-06-14 17:59:09 -0400

    Amusement Ride app

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  
+  resources :attractions#, only: [:index, :show, :new, :create, :edit, :update, :destroy]
+  resources :users#, only: [:index, :show, :new, :create, :edit, :update, :destroy]
+
 end
